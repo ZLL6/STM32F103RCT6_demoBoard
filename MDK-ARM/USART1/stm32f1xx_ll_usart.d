89a94d024559d6be05580f866aa45081fcafef61
Eller <Mr.ZhongLi@outlook.com>
2022-10-08 22:20:25 +0800

    first commit

---
+++ MDK-ARM/USART1/stm32f1xx_ll_usart.d
@@ -0,0 +1,12 @@
+usart1\stm32f1xx_ll_usart.o: ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usart.c
+usart1\stm32f1xx_ll_usart.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usart.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/CMSIS/Include/core_cm3.h
+usart1\stm32f1xx_ll_usart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/CMSIS/Include/cmsis_version.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_rcc.h
+usart1\stm32f1xx_ll_usart.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_bus.h
---
+++ MDK-ARM/USART1/system_stm32f1xx.d
@@ -0,0 +1,9 @@
+usart1\system_stm32f1xx.o: ../Core/Src/system_stm32f1xx.c
+usart1\system_stm32f1xx.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+usart1\system_stm32f1xx.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+usart1\system_stm32f1xx.o: ../Drivers/CMSIS/Include/core_cm3.h
+usart1\system_stm32f1xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+usart1\system_stm32f1xx.o: ../Drivers/CMSIS/Include/cmsis_version.h
+usart1\system_stm32f1xx.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+usart1\system_stm32f1xx.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+usart1\system_stm32f1xx.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
